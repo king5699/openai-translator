5c17b96f6e9b23d498fe9843a6ac767ed5c867e2
king5699 <735018963@qq.com>
2024-03-24 16:28:05 +0800

    clean code

--- app.py
+++ app.py
@@ -14,41 +14,6 @@ headers = {
 block_break = '\n========== [END] ==========\n\n'
 
 
-def translate(source_text):
-    if not source_text.strip():
-        gr.Warning('提示词不能为空')
-        return ''
-    try:
-        target_text = []
-        prompts = source_text.split(block_break)
-        for prompt in prompts:
-            if prompt.startswith(headers[ContentType.TEXT.value]):
-                offset = len(headers[ContentType.TEXT.value])
-            elif prompt.startswith(headers[ContentType.TABLE.value]):
-                offset = len(headers[ContentType.TABLE.value])
-            else:
-                continue
-            translation, status = model.make_request(prompt[offset:].strip())
-            if status:
-                target_text.append(translation)
-        return '\n\n'.join(target_text)
-    except Exception as e:
-        gr.Error(f'发生异常：{e}')
-        return ''
-
-
-def upload_source_file(language, file_path):
-    book = pdf_parser.parse_pdf(file_path, pages=3)
-    source_text = []
-    for page in book.pages:
-        for content in page.contents:
-            source_text.append(headers[content.content_type.value])
-            prompt = model.translate_prompt(content, language)
-            source_text.append(prompt)
-            source_text.append(block_break)
-    return ''.join(source_text)
-
-
 with gr.Blocks() as demo:
     gr.Markdown("""
     ## PDF文档在线智能翻译器
@@ -67,10 +32,44 @@ with gr.Blocks() as demo:
 
             target_textbox = gr.Textbox(label='译文')
 
-    source_file.upload(upload_source_file,
-                       inputs=[language_dropdown, source_file],
-                       outputs=source_textbox,
-                       trigger_mode='always_last')
-    submit_btn.click(translate, inputs=source_textbox, outputs=target_textbox)
+    @gr.on(triggers=[language_dropdown.change, source_file.upload],
+           inputs=[language_dropdown, source_file],
+           outputs=source_textbox)
+    def upload_source_file(language, file_path):
+        if not file_path:
+            return ''
+        book = pdf_parser.parse_pdf(file_path, pages=3)
+        source_text = []
+        for page in book.pages:
+            for content in page.contents:
+                source_text.append(headers[content.content_type.value])
+                prompt = model.translate_prompt(content, language)
+                source_text.append(prompt)
+                source_text.append(block_break)
+        return ''.join(source_text)
+
+    @gr.on(triggers=[submit_btn.click], inputs=source_textbox, outputs=target_textbox)
+    def translate(source_text):
+        if not source_text.strip():
+            gr.Warning('提示词不能为空')
+            return ''
+        try:
+            target_text = []
+            prompts = source_text.split(block_break)
+            for prompt in prompts:
+                if prompt.startswith(headers[ContentType.TEXT.value]):
+                    offset = len(headers[ContentType.TEXT.value])
+                elif prompt.startswith(headers[ContentType.TABLE.value]):
+                    offset = len(headers[ContentType.TABLE.value])
+                else:
+                    continue
+                translation, status = model.make_request(prompt[offset:].strip())
+                if status:
+                    target_text.append(translation)
+            return '\n\n'.join(target_text)
+        except Exception as e:
+            gr.Error(f'发生异常：{e}')
+            return ''
+
 
 demo.launch(server_name='0.0.0.0')
